68e5cec7aa8f7d21452d5a9c611ccfe3d16b433b
noman2047 <nomancse101@gmail.com>
2022-08-23 17:22:57 +0600

    calculation Part

--- js/index.js
+++ js/index.js
@@ -1,12 +1,51 @@
+//checking Validation it is Number Or Not
+function isItNumber(numberOrNot){
+  if(isNaN(numberOrNot)){
+    alert("Please Enter Number.");
+  }
+  else{
+    return numberOrNot;
+  }
+
+}
+//Total player,manager and coach result
 function perPlayer(totalplayer){
   document.getElementById('perPlayer').addEventListener('keyup',function(item){
     const perPlayerRate=item.target.value;
-    const totalResult=perPlayerRate * totalplayer;
+    const validationCheck=isItNumber(perPlayerRate);
+    const totalPlayerCost=validationCheck * totalplayer;
     document.getElementById('calculat').addEventListener('click',function(){
-      document.getElementById('totalRate').innerText=totalResult;
+      document.getElementById('totalRate').innerText=totalPlayerCost;
     })
   })
+  document.getElementById('coach').addEventListener('keyup',function (item){
+    const coachBilleni=document.getElementById('coach');
+    const perPlayerRate=item.target.value;
+    const coachBill=isItNumber(perPlayerRate);
+    coachBilleni.value=coachBill;
+  })
+  
+  document.getElementById('manager').addEventListener('keyup',function (item){
+    const managerBilleni=document.getElementById('manager');
+    const perPlayerRate=item.target.value;
+    const managerBill=isItNumber(perPlayerRate);
+    managerBilleni.value=managerBill;
+  })
 }
+
+document.getElementById('calculateTotal').addEventListener('click',function(){
+  const getCoachValue=document.getElementById('coach');
+  const getmanagerValue=document.getElementById('manager');
+  const getplayerValue=document.getElementById('totalRate');
+
+  const getCoachValuefloat=parseFloat(getCoachValue.value);
+  const getmanagerValuefloat=parseFloat(getmanagerValue.value);
+  const getplayerValuefloat=parseFloat(getplayerValue.innerText);
+  const totalResult=getCoachValuefloat+getmanagerValuefloat+getplayerValuefloat;
+  const updatedata=document.getElementById('update');
+  updatedata.innerText=totalResult;
+
+})
 
 const players=[];
 function addToPlayer(player){
